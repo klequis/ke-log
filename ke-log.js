41b41c1a62d00eb4acfe0872a6927b5d2cf3845f
klequis <klequis@gmail.com>
2018-07-14 13:42:53 -0700

    Update ke-log.js

--- ke-log.js
+++ ke-log.js
@@ -1,76 +1,38 @@
-const styleRed = [
-  'color: #ff0000',
-  'font-weight: bold',
-].join(';');
+import chalk from 'chalk'
+const log = console.log
 
-const styleGreen = [
-  'color: #00ff00',
-  'font-weight: bold',
-].join(';');
-
-const styleBlue = [
-  'color: #00ced1',
-  'font-weight: bold',
-].join(';');
-
-const styleOrange = [
-  'color: #ffa500',
-  'font-weight: bold',
-].join(';');
-
-const stylePink = [
-  'color: #ff69b4',
-  'font-weight: bold',
-].join(';');
-const styleYellow = [
-  'color: #ffd700',
-  'font-weight: bold',
-].join(';');
-
-const getStyle = (styleName) => {
-  let color;
-  switch (styleName) {
-    case 'red':
-      color = styleRed;
-      break;
-    case 'blue':
-      color = styleBlue;
-      break;
-    case 'green':
-      color = styleGreen;
-      break;
-    case 'orange':
-      color = styleOrange;
-      break;
-    case 'pink':
-      color = stylePink;
-      break;
-    case 'yellow':
-      color = styleYellow
-      break;
-    default:
-      color = '';
-  }
-
-  return color;
-}
-
-export const log = (message = '', value = '', color = '') => {
-  let count = 3
-  if (color === '') { count-- }
-  if (value === '') { count-- }
-
-  let nMessage
-  if (count === 1) {
-    nMessage = '[log]'
-    value = message
+const checkValue = (value) => {
+  if (value === undefined) {
+    return ''
   } else {
-    nMessage = `%c[${message}]`
+    return value
   }
-  const style = getStyle(color);
-
-  // eslint-disable-next-line no-console
-  console.log(nMessage, style, value);
 }
 
-export default { log }
+export const red = (message, value) => {
+   log(chalk.bgRed(` ${message} `), checkValue(value))
+}
+export const green = (message, value) => {
+   log(chalk.bgGreen(` ${message} `), checkValue(value))
+}
+export const yellow = (message, value) => {
+   // log(chalk.bgYellow(` ${message} `), checkValue(value))
+  log(chalk.bgYellow(chalk.black(` ${message} `)), checkValue(value))
+}
+export const blue = (message, value) => {
+   log(chalk.bgBlue(` ${message} `), checkValue(value))
+}
+
+export const redf = (message, value) => {
+   log(chalk.red(`${message}`), checkValue(value))
+}
+export const greenf = (message, value) => {
+   log(chalk.green(`${message}`), checkValue(value))
+}
+export const yellowf = (message, value) => {
+   log(chalk.yellow(`${message}`), checkValue(value))
+}
+export const bluef = (message, value) => {
+   log(chalk.blue(`${message}`), checkValue(value))
+}
+export default { red, green, yellow, blue, redf, greenf, yellowf, bluef }
